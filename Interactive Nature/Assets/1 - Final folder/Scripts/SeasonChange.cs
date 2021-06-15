5b425703c517c34c8ee22bfad4d3fbad77a66d77
Kristopherius <kristopherius@gmail.com>
2021-06-15 12:27:57 +0200

    made a season change function for the ar screen

---
+++ Interactive Nature/Assets/1 - Final folder/Scripts/SeasonChange.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeasonChange : MonoBehaviour
+{
+    GameObject myPlant;
+    private void Start()
+    {
+        if(myPlant == null)
+        {
+            foreach(Transform child in transform)
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (myPlant != null)
+        {
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(true);
+            }
+        }
+    }
+    public void changePlant(GameObject plant)
+    {
+        myPlant = plant;
+    }
+
+    public void seasonChange(string season)
+    {
+        foreach(Transform child in myPlant.transform)
+        {
+            if (child.name.Contains(season))
+            {
+                child.gameObject.SetActive(true);
+            }
+            else
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
+    }
+}
